699a26c96bbb3e40043614ec8f24dacb1267a27d
quaeast <424044876@qq.com>
2019-12-26 11:00:07 +0800

    top level

--- app/src/main/java/bjfu/it/fangzidong/starbuzz/TopLevelActivity.java
+++ app/src/main/java/bjfu/it/fangzidong/starbuzz/TopLevelActivity.java
@@ -3,25 +3,41 @@ package bjfu.it.fangzidong.starbuzz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.CursorAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public class TopLevelActivity extends AppCompatActivity {
+
+    private Cursor favoritesCursor;
+    private SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
 
+        setupOptionsListView();
+        setupFavoritesListView();
+    }
+
+    private void setupOptionsListView() {
         AdapterView.OnItemClickListener itemClickListener =
                 new OnItemClickListener() {
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        if(position == 0){
+                        if (position == 0) {
                             Intent intent = new Intent(TopLevelActivity.this,
                                     DrinkCategoryActivity.class);
                             startActivity(intent);
@@ -31,5 +47,36 @@ public class TopLevelActivity extends AppCompatActivity {
 
         ListView listView = findViewById(R.id.list_options);
         listView.setOnItemClickListener(itemClickListener);
+    }
+
+    private void setupFavoritesListView() {
+        ListView listFavorites = findViewById(R.id.list_favourite);
+        final SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+        try {
+            db = starbuzzDatabaseHelper.getReadableDatabase();
+            favoritesCursor = db.query("DRINK",
+                    new String[]{"_id","NAME"},
+                    "FAVOURITE=1",
+                    null,null,null,null);
+            CursorAdapter favoriteAdapter =
+                    new SimpleCursorAdapter(TopLevelActivity.this,
+                            android.R.layout.simple_list_item_1, favoritesCursor,
+                            new String[]{"NAME"},
+                            new int[]{android.R.id.text1}, 0);
+            listFavorites.setAdapter(favoriteAdapter);
+        }catch (SQLiteException e){
+            Log.e("sqlite", e.getMessage());
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+        listFavorites.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(TopLevelActivity.this, DrinkActivity.class);
+                intent.putExtra(DrinkActivity.EXTRA_DRINKID, (int)id);
+                startActivity(intent);
+            }
+        });
     }
 }
